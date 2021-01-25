b333c700ce602c23fab785fd7ec97aee43a8f3d5
Alex Gotfrid <alexander.gotfrid@boldcommerce.com>
2021-01-25 17:27:19 -0600

    Node example plugin is not verifying signatures correctly in the middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,22 +1,28 @@
-const httpsig = require('http-signature');
-
+const httpSignature = require('http-signature');
 const verify_signature = (req, res, next) => {
     try {
         // verify Cashier's HTTP signature
+
+        // a workaround to avoid failing validation if date header contains `request-target`
+        const tempURL = req.url;
+        req.url = req.originalUrl;
+        const parsed = httpSignature.parse(req);
+        req.url = tempURL;
+
         // this is required in order to verify that the request originated from Cashier
-        const parsed = httpsig.parse(req);
-        httpsig.verifyHMAC(parsed, process.env.CASHIER_CLIENT_SECRET);
-        next();
+        if (httpSignature.verifyHMAC(parsed, process.env.CASHIER_CLIENT_SECRET)) {
+            next();
+        } else {
+            res.status(401).end();
+        }
     } catch (error) {
         res.status(401).end();
     }
 };
-
 const log = (req, res, next) => {
     console.log(req.method, req.url);
     next();
 };
-
 module.exports = {
     log,
     verify_signature,
