9bb6f935307f28db12ca22fe90b8d8bf7632f1a8
Muhammad Umer Draz <mumdrsa@hotmail.com>
2021-06-17 22:12:55 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Example app listening on port ');
-    console.log(port);
+   // console.log('Example app listening on port ');
+   // console.log(port);
     
 });
